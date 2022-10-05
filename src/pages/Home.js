63fa0e86377d6d31fd9704aa11b7dd7eb4a02666
montescar19dev <carlosmontes19.dev@gmail.com>
2022-10-05 19:39:12 -0400

    cambios products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getProducts, filterProducts, filterCategory } from "../store/slices/products.slices";
+import {
+  getProducts,
+  filterProducts,
+  filterCategory,
+} from "../store/slices/products.slices";
 import "../assests/styles/home.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Accordion } from 'react-bootstrap';
-import { addToCart } from '../store/slices/cart.slices';
+import { Accordion } from "react-bootstrap";
+import { addToCart } from "../store/slices/cart.slices";
 
 const baseUrl = "https://ecommerce-api-react.herokuapp.com/api/v1";
 
@@ -41,81 +45,89 @@ const Home = () => {
     navigate(`/product/${id}`);
   };
 
-  const addCart = (id)=>{
-    const productToAdd={
-        id:id,
-        quantity:1
-    }
+  const addCart = (id) => {
+    const productToAdd = {
+      id: id,
+      quantity: 1,
+    };
     dispatch(addToCart(productToAdd));
-}
+  };
 
   return (
-    <div className="products-container ">
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          id="search-bar"
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
-          className="form-control"
-          placeholder="Search"
-        />
-        <button
-          id="search-bar"
-          onClick={getFilterProducts}
-          className=" btn btn-outline-info"
-        >
-          Search
-        </button>
-      </div>
-      <div className="categories-container">
-        
-        <Accordion defaultActiveKey="1" style={{width: "fit-content"}}>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header> <i className="fa-solid fa-filter"></i></Accordion.Header>
-            <Accordion.Body>
+    <>
+      <div className="products-container ">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            id="search-bar"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            className="form-control"
+            placeholder="Search"
+          />
+          <button
+            id="search-bar"
+            onClick={getFilterProducts}
+            className=" btn btn-outline-info"
+          >
+            Search
+          </button>
+        </div>
+        <div className="categories-container">
+          <Accordion defaultActiveKey="1" style={{ width: "fit-content" }}>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                {" "}
+                <i className="fa-solid fa-filter"></i>
+              </Accordion.Header>
+              <Accordion.Body>
                 {categories.map((category) => (
+                  <div
+                    key={category.id}
+                    onClick={() => categoryFilter(category.id)}
+                  >
+                    {category.name}
+                  </div>
+                ))}
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </div>
+        <div className="cards-container">
+          {products.map((product) => (
+            <li key={product.id} className="product-wrapper">
+              <div className="product-container">
                 <div
-                key={category.id}
-                onClick={() => categoryFilter(category.id)}
-                
+                  className="product-image"
+                  onClick={() => goDetail(product.id)}
                 >
-                {category.name}
+                  <img src={product.productImgs[0]} alt="" />
                 </div>
-            ))}
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-      </div>
-      <div className="cards-container">
-        {products.map((product) => (
-          <li key={product.id} className="product-wrapper">
-            <div className="product-container">
-              <div
-                className="product-image"
-                onClick={() => goDetail(product.id)}
-              >
-                <img src={product.productImgs[0]} alt="" />
-              </div>
 
-              <div className="product-info">
-                <h3>{product.title}</h3>
-                <div>
-                  Price:
-                  <p>
-                    <b>$ {product.price}</b>
-                  </p>
+                <div className="product-info">
+                  <h3>{product.title}</h3>
+                  <div>
+                    Price:
+                    <p>
+                      <b>$ {product.price}</b>
+                    </p>
+                  </div>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => addCart(product.id)}
+                  >
+                    Buy
+                  </button>
                 </div>
-                <button className="btn btn-primary" onClick={() => addCart(product.id)}>Buy</button>
               </div>
-            </div>
-          </li>
-        ))}
-        <footer className="footer">
-          <h1>Hecho en Academlo</h1>
-        </footer>
+            </li>
+          ))}
+        </div>
       </div>
-    </div>
+      <footer className="footer">
+        <h1>Hecho en Academlo</h1>
+      </footer>
+    </>
   );
 };
 
