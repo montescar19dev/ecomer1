0cbd317c088481889ee81e68ed48693dddf41b90
montescar19dev <carlosmontes19.dev@gmail.com>
2022-10-05 23:09:44 -0400

    cambios generales

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
     return (
         <div className='login-wrapper'>
             <div className='login-container'>
-                <h2>Welcome! Enter your email and password to continue.</h2>
+                <h2>Welcome! <br/> Enter your email and password to continue.</h2>
                 <div className='testData-container'>
                     <h5>Test data</h5>
                     <div>
