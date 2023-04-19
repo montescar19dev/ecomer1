ea6c1ba0dd62bdce211be77de390b42f3a29e351
Carlos Augusto Montes Gil <110144806+montescar19dev@users.noreply.github.com>
2023-04-19 17:28:46 -0400

    Update products.slices.js

--- src/store/slices/products.slices.js
+++ src/store/slices/products.slices.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { setIsloading } from './isLoading.slice';
 
-const baseUrl = 'https://ecommerce-api-react.herokuapp.com/api/v1'
+const baseUrl = 'https://e-commerce-api.academlo.tech/api/v1'
 
 
 export const productsSlice = createSlice({
